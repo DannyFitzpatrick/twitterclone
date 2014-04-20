4f072fa1525ca4a2768c7df236e11acc177f0025
Danny Fitzpatrick <dfitzpatrick1991@gmail.com>
2014-04-20 17:23:16 +0100

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BirdfeedApp::Application.config.secret_key_base = '8be5bafce1a79971ccc885c8a1da0184213cd515b63fa574398a3d9f84fb033ebc61e953111735f3de1f89ace7b4fe4972ff24f269da97c14a8c9799a0ab87df'
