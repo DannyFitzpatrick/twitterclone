266c983ba4c620162de2263b3c7685d7b18e948c
Danny Fitzpatrick <dfitzpatrick1991@gmail.com>
2014-04-20 17:58:04 +0100

    created user relationships infrastructure

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,24 @@ class User < ActiveRecord::Base
 		validates :username, uniqueness: true, presence: true
 		validates :email, uniqueness: true, presence: true, format: { with: /\z[\w.+-]+@([\w]+.)+\w+\A/ }
 
-	private
+		has_many :ribbits
+		has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
+		has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+			has_many :followers, through: :follower_relationships
+			has_many :followeds, through: :followed_relationships
+
+		def following? user
+   		    self.followeds.include? user
+		end
  
-	def prep_email
-    	self.email = self.email.strip.downcase if self.email
-	end
+		def follow user
+    		Relationship.create follower_id: self.id, followed_id: user.id
+		end
+
+		private
+ 
+		def prep_email
+    		self.email = self.email.strip.downcase if self.email
+		end
 
 end
